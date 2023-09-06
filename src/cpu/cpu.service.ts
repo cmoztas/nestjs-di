ef1048ca24545e88f460d3dba9d01dbe669cd747
cagkanoztas <cagkan.oztas@jengal.com>
2023-09-06 16:27:23 +0300

    more on DI between modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -7,4 +7,11 @@ export class CpuService {
     private powerService: PowerService
   ) {
   }
+
+  compute(a: number, b: number) {
+    console.log(`Drawing 10 watts of power from Power Service`);
+    this.powerService.supplyPower(10);
+
+    return a + b;
+  }
 }
